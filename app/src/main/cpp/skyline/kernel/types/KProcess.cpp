9afae045ec09394b34f2c4ba4d8c39be578e3722
lynxnb <niccolo.betto@gmail.com>
2023-12-11 18:59:31 +0100

    KProcess: correctly handle empty optional chunk

--- app/src/main/cpp/skyline/kernel/types/KProcess.cpp
+++ app/src/main/cpp/skyline/kernel/types/KProcess.cpp
@@ -67,16 +67,17 @@ namespace skyline::kernel::type {
         bool isAllocated{};
 
         u8 *pageCandidate{state.process->memory.tlsIo.guest.data()};
-        std::pair<u8 *, ChunkDescriptor> chunk;
         while (state.process->memory.tlsIo.guest.contains(span<u8>(pageCandidate, constant::PageSize))) {
-            chunk = memory.GetChunk(pageCandidate).value();
+            auto chunk = memory.GetChunk(pageCandidate);
+            if (!chunk)
+                continue;
 
-            if (chunk.second.state == memory::states::Unmapped) {
+            if (chunk->second.state == memory::states::Unmapped) {
                 memory.MapThreadLocalMemory(span<u8>{pageCandidate, constant::PageSize});
                 isAllocated = true;
                 break;
             } else {
-                pageCandidate = chunk.first + chunk.second.size;
+                pageCandidate = chunk->first + chunk->second.size;
             }
         }
 
@@ -96,16 +97,17 @@ namespace skyline::kernel::type {
             bool isAllocated{};
 
             u8 *pageCandidate{memory.stack.guest.data()};
-            std::pair<u8 *, ChunkDescriptor> chunk;
             while (state.process->memory.stack.guest.contains(span<u8>(pageCandidate, state.process->npdm.meta.mainThreadStackSize))) {
-                chunk = memory.GetChunk(pageCandidate).value();
+                auto chunk{memory.GetChunk(pageCandidate)};
+                if (!chunk)
+                    continue;
 
-                if (chunk.second.state == memory::states::Unmapped && chunk.second.size >= state.process->npdm.meta.mainThreadStackSize) {
+                if (chunk->second.state == memory::states::Unmapped && chunk->second.size >= state.process->npdm.meta.mainThreadStackSize) {
                     memory.MapStackMemory(span<u8>{pageCandidate, state.process->npdm.meta.mainThreadStackSize});
                     isAllocated = true;
                     break;
                 } else {
-                    pageCandidate = chunk.first + chunk.second.size;
+                    pageCandidate = chunk->first + chunk->second.size;
                 }
             }
 
